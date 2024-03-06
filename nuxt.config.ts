f60d0166121bf4982b923887717255fcbb746abb
MoidulHasan <antu.khan.988@gmail.com>
2024-03-07 02:13:05 +0600

    feat: eslint and prettier configuration added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import path from "path";
+import path from "node:path";
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  srcDir: 'src/',
-  css: ['~/assets/styles/scss/main.scss'],
-  modules: [
-    'nuxt-primevue',
-    '@nuxtjs/tailwindcss'
-  ],
+  srcDir: "src/",
+  css: ["~/assets/styles/scss/main.scss"],
+  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss", "@nuxtjs/eslint-module"],
   primevue: {
     unstyled: true,
-    importPT: { from: path.resolve(__dirname, './src/assets/styles/presets/lara/') }      //import and apply preset
+    importPT: {
+      from: path.resolve(__dirname, "./src/assets/styles/presets/lara/"),
+    },
   },
   tailwindcss: {
     exposeConfig: true,
   },
-
-})
+});
