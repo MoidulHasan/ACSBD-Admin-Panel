ad68a5494fa4d2589055eb6040a7b7bedba82c88
MoidulHasan <antu.khan.988@gmail.com>
2024-05-18 07:14:40 +0600

    feat: meta title template added to app

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (title) => `${title} | AC Service BD Admin Panel`,
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
-<script setup lang="ts"></script>
