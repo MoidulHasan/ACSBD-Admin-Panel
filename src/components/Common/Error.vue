f3fb1a1f3af3fef13242070915645ac12ae0aea8
MoidulHasan <antu.khan.988@gmail.com>
2024-06-02 23:43:45 +0600

    feat: common error component created

---
+++ src/components/Common/Error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps({
+  error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+  <div class="h-full flex flex-col justify-center items-center">
+    <h1>{{ error.statusCode }}</h1>
+    <p>{{ error.statusMessage }}</p>
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
+</template>
+
+<style scoped></style>
