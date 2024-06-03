73256283599b33be6fa642bc8bf926d6e03afab9
MoidulHasan <antu.khan.988@gmail.com>
2024-06-04 01:14:52 +0600

    feat: create new product page created

---
+++ src/pages/products/create-new.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: "Create New | Products",
+});
+
+definePageMeta({
+  name: "product-create-new",
+});
+</script>
+
+<template>
+  <div>create new product</div>
+</template>
+
+<style scoped></style>
