d69cf77b14d454c536b586e98264884a24fae101
MoidulHasan <antu.khan.988@gmail.com>
2024-06-04 01:15:03 +0600

    feat: edit product page created

---
+++ src/pages/products/[slug]/edit.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: "Edit | Products",
+});
+
+definePageMeta({
+  name: "product-edit",
+});
+</script>
+
+<template>
+  <div>edit product</div>
+</template>
+
+<style scoped></style>
