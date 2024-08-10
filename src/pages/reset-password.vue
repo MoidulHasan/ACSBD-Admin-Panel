21eb9caa0bcb54ce21cce44d2251903d68144f6c
MoidulHasan <antu.khan.988@gmail.com>
2024-08-10 23:43:34 +0600

    feat: reset password page created

---
+++ src/pages/reset-password.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  title: "Reset Password",
+  layout: "empty",
+});
+</script>
+
+<template>
+  <div>
+    <CommonCommingSoon />
+  </div>
+</template>
+
+<style scoped></style>
